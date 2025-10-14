f0fa4db15a33dd21069b92bb6f71de8e67e8b92b
gabriel lennon <gabriellennon7@gmail.com>
2025-10-14 10:57:37 -0300

    feature: add new json schema transform to transfor file

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,3 +1,5 @@
+import { db } from '@/infra/db';
+import { uploads } from '@/infra/db/schemas/uploads';
 import { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
 import z from 'zod';
 
@@ -5,16 +7,25 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
     server.post('/uploads', {
         schema: {
             summary: 'Upload an image',
-            body: z.object({
-                name: z.string(),
-                password: z.string().optional()
-            }),
+            consumes: ['multipart/form-data'],
             response: {
                 201: z.object({ uploadId: z.string() }),
                 409: z.object({ message: z.string() }).describe('Upload already exists'),
             }
         }
     }, async (request, reply) => {
+        const uploadedFile = await request.file({
+            limits: {
+                fileSize: 1024 * 1024 * 2, // 2MB
+            }
+        })
+
+
+        await db.insert(uploads).values({
+            name: 'Teste',
+            remoteKey: 'Teste',
+            remoteUrl: 'Teste',
+        })
         return reply.status(201).send({ uploadId: 'teste' })
     })
 
--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,10 +1,11 @@
 import { fastify } from "fastify";
 import { fastifyCors } from "@fastify/cors"
-import { serializerCompiler, validatorCompiler, hasZodFastifySchemaValidationErrors, jsonSchemaTransform} from 'fastify-type-provider-zod'
+import { serializerCompiler, validatorCompiler, hasZodFastifySchemaValidationErrors } from 'fastify-type-provider-zod'
 import { uploadImageRoute } from "./routes/upload-image";
 import fastifyMultipart from "@fastify/multipart";
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUi from "@fastify/swagger-ui";
+import { transformSwaggerSchema } from "./transform-swagger-schema";
 
 const server = fastify();
 
@@ -40,7 +41,7 @@ server.register(fastifySwagger, {
             version: '1.0.0',
         },
     },
-    transform: jsonSchemaTransform
+    transform: transformSwaggerSchema
 })
 
 server.register(fastifySwaggerUi, {
